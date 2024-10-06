87b30ee7d91c06e6d24e60fb7c514fd101f3d940
EnchantedDonutStudioz <EnchantedDonutStudioz@gmail.com>
2024-10-06 17:55:18 -0400

    takeealookie

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
 import { ChemicalServer } from "chemicaljs";
 import express from "express";
+import basicAuth from "express-basic-auth";
+
 
 const [app, listen] = new ChemicalServer();
 const port = process.env.PORT || 8080;
 
+
+app.use(basicAuth({
+    users: { 'admin': 'password' },
+    challenge: true,
+    unauthorizedResponse: (req, res) => {
+        if (res) {
+            res.sendFile(path.join(__dirname, "auth.html"));
+        } else {
+            console.error("Response object is undefined");
+        }
+    },
+}));
 app.use(express.static("public", {
     index: "index.html",
     extensions: ["html"]
 }));
+
+app.get('/success', (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get('/', (req, res) => {
+    res.redirect('/success');
+});
 
 app.serveChemical();
 
